232df76fb8b97a404c5ef2e879c2c357b2a8150d
Antoine LUCAS <a-lucas@users.noreply.github.com>
2016-09-19 12:35:52 +0700

    Initial commit 1.0.0

--- test/karma-unit.conf.js
+++ test/karma-unit.conf.js
@@ -3,6 +3,17 @@ var sharedConfig = require('./karma-shared.conf');
 module.exports = function(config) {
   var conf = sharedConfig();
 
+  conf.customLaunchers= {
+    Chrome_travis_ci: {
+      base: 'Chrome',
+          flags: ['--no-sandbox']
+    }
+  };
+
+  if (process.env.TRAVIS) {
+    conf.browsers = ['Chrome_travis_ci'];
+  }
+
   conf.files = conf.files.concat([
     //extra testing code
     'bower_components/angular-mocks/angular-mocks.js',
